144d6597a672b682833e8c8cb03bba11b40aac72
Matheusilva431 <matheusilva431+github@gmail.com>
2023-10-11 20:43:57 -0300

    aula September 30, 2023

--- workspace_eclipse/lab_jpa/src/main/java/br/inatel/labs/labjpa/service/ProdutoService.java
+++ workspace_eclipse/lab_jpa/src/main/java/br/inatel/labs/labjpa/service/ProdutoService.java
@@ -1,11 +1,14 @@
 package br.inatel.labs.labjpa.service;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.inatel.labs.labjpa.entity.Produto;
+import br.inatel.labs.labjpa.repository.ProdutoRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 
@@ -13,26 +16,22 @@ import jakarta.persistence.PersistenceContext;
 @Transactional
 public class ProdutoService {
 	
-	@PersistenceContext
-	private EntityManager em;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public Produto salvar(Produto p) {
-		p = em.merge(p);
-		return p;
+		return produtoRepository.save(p);
 	}
 	
-	public Produto buscarPeloId(Long id) {
-		Produto produtoEncontrado = em.find(Produto.class, id);
-		return produtoEncontrado;
+	public Optional<Produto> buscarPeloId(Long id) {
+		return produtoRepository.findById(id);
 	}
 	
 	public List<Produto> listar() {
-		List<Produto> listaProdutos = em.createQuery("select p from Produto p", Produto.class).getResultList();
-		return listaProdutos;
+		return produtoRepository.findAll();
 	}
 	
 	public void remove(Produto p) {
-		p = em.merge(p);
-		em.remove(p);
+		produtoRepository.delete(p);
 	}
 }
